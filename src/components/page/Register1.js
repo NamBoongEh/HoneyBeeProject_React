a5abf37a2b55a252b08d964af8d8cc2930bf4e6e
NamBoongEh <skathdus0916@gmail.com>
2021-03-28 01:32:19 +0900

    3/28 TOS done, Register1 layout done

--- src/components/page/Register1.js
+++ src/components/page/Register1.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+
+const Links = styled(Link)`
+  float: right;
+  font-size: smaller;
+  color: slategray;
+  text-decoration: underline;
+  margin-right: 10px;
+  line-height: 50px;
+  &:hover{
+    color: slategray;
+  }
+  `
 
 export default function Register1(){
     return(
-    <>
-    <div>회원가입</div>
-    </>
+        <div style={{backgroundColor:"rgb(246,218,66)", padding:"60px"}}>
+            <div className={"container"} style={{backgroundColor:"white",padding:"40px 40px 60px 40px", height:"50%", width:"50%", boxShadow:"0 10px 15px 0 rgba(0, 0, 0, 0.5)"}}>
+                <h3 style={{fontWeight:"bold"}}>회원가입</h3>
+                <h4 style={{fontWeight:"bold", color:"#4B89DC", paddingTop:"40px"}}>필수정보</h4>
+
+
+
+                <div style={{paddingTop:"50px"}}>
+                    <button style={{backgroundColor:"orange", color:"white", border:"none", fontSize:"18px", padding:"10px 20px 10px 20px", borderRadius:"10px"}}>뒤로가기</button>
+                    <button style={{backgroundColor:"orange", color:"white", border:"none", fontSize:"18px", padding:"10px 20px 10px 20px", borderRadius:"10px", float:"right"}}>다음</button>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/components/page/TermsOfService.js
+++ src/components/page/TermsOfService.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import {EmojiSmile, x} from "react-bootstrap-icons";
+import {X} from "react-bootstrap-icons";
 import styled from "styled-components";
-import {x} from "react-bootstrap-icons";
 
-const StyledLogin = styled(EmojiSmile)`
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width: 50%;
+const ButtonX = styled(X)`
+    font-size: xx-large;
+    display: inline-block;
+    float: right;
+    &:hover{
+      cursor: pointer;
+    }
  `
 
 export default function TermsOfService(){
     return(
         <>
             <div style={{backgroundColor:"rgba(0, 0, 0, 0.15)", padding:"10%"}}>
-                <div className={"container"} style={{backgroundColor:"white",padding:"25px 40px 60px 30px", height:"80%", width:"42%", boxShadow:"0 10px 15px 0 rgba(0, 0, 0, 0.5)",
+                <div className={"container"} style={{backgroundColor:"white",padding:"25px 40px 60px 30px", height:"80%", width:"50%", boxShadow:"0 10px 15px 0 rgba(0, 0, 0, 0.3)",
                 borderWidth:"6px", borderColor:"#4B89DC", borderStyle:"solid", lineHeight:"200%"}}>
                     <div style={{fontSize:"18px", fontWeight:"bold"}}>
                         이용약관 동의
+                        <ButtonX/><br/>
                     </div>
-                    <div className={"container"} style={{borderWidth:"1px", borderColor:"#DDDDDD", borderStyle:"solid", width:"85%", lineHeight:"300%"}}>
-                        안녕하세요
+                    <div className={"container"} style={{borderWidth:"1px", borderColor:"#DDDDDD", borderStyle:"solid", padding:"0 0 20px 10px", width:"98%", lineHeight:"130%"}}>
+                        <div style={{color:"#4b89dc", fontWeight:"bold", lineHeight:"280%"}}>이용약관<br/></div>
+                        안녕하세요<br/>
+                        여기에 글을 쓸겁니다. 이걸 서명하면 당신은 노예가 되는겁니다.<br/>
                     </div>
                 </div>
             </div>
